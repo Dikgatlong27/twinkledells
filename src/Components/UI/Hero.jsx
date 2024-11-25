fc4c4bbf791b80a6a4b025594bca2c020a8994fe
Tebogo Dikgatlong <tpdikgatlong@gmail.com>
2024-11-25 11:15:52 +0200

    modifying Nav

--- src/Components/UI/Hero.jsx
+++ src/Components/UI/Hero.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import './Hero.css'
+
 const Hero = () => {
   return (
-    <div>
-      <h1>Hero</h1>
+    <div className="hero">
+      <div className="hero-overlay">
+        <h1>Twinkle Dells Florist</h1>
+        <p>Bringing Nature's Beauty to Your Doorstep</p>
+        <p>Opening Soon!</p>
+        <button><a href='#' target='_blank' rel="noreferrer">Visit Us</a></button>
+      </div>
     </div>
   )
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,10 +4,7 @@ import Hero from '../Components/UI/Hero'
 const Home = () => {
   return (
     <div>
-      <h1 className='main-section'><Hero /></h1>
-      <h1 className='section'>Section</h1>
-      <h1 className='main-section'>Section</h1>
-      <h1 className='section'>Section</h1>
+      <Hero />
     </div>
   )
 }
